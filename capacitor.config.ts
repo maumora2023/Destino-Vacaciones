a5ce2c712c00a7b4029982b145615898a59c1f4d
maumora2023 <147200642+maumora2023@users.noreply.github.com>
2024-05-07 08:56:20 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'destinos.vacacionales',
+  appName: 'destinos-vacacionales',
+  webDir: 'www'
+};
+
+export default config;
